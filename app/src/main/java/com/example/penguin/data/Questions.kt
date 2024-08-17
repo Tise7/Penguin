f1a171356fee9d380720d1a1484a814afa2fcfe8
Tise7 <oluwatisejudah7@gmail.com>
2024-08-17 14:23:10 +0300

    Updated

--- app/src/main/java/com/example/penguin/data/Questions.kt
+++ app/src/main/java/com/example/penguin/data/Questions.kt
@@ -7,11 +7,3 @@ data class Question(
     @StringRes val options: List<Int>,
     @StringRes val correctAnswer: Int
 )
-
-//data class QuizUiState(
-//    val questions: List<Question> = emptyList(),
-//    val currentQuestionIndex: Int = 0,
-//    val selectedOptionIndex: Int? = null,
-//    val score: Int = 0,
-//    val showResult: Boolean = false
-//)
