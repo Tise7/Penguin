70c497f401c589d2287a21b87aa652df152fd285
Tise7 <oluwatisejudah7@gmail.com>
2024-08-17 18:56:30 +0300

    Updates

--- app/src/main/java/com/example/penguin/utils/WindowStateUtils.kt
+++ app/src/main/java/com/example/penguin/utils/WindowStateUtils.kt
@@ -1,14 +1,10 @@
 package com.example.penguin.utils
 
-import androidx.annotation.StringRes
-import com.example.penguin.R
 
-
-enum class PenguinScreens (@StringRes val title: Int) {
-    Welcome(title = R.string.welcome),
-    PenguinList(title = R.string.Penguins),
-    PenguinDetail(title = R.string.Penguin_Detail),
-    Quiz(title = R.string.Quiz),
-    PenguinGame(title = R.string.Penguin_Catch)
-
+enum class PenguinScreens {
+    Welcome,
+    PenguinList,
+    PenguinDetail,
+    Quiz,
+    PenguinGame
 }
